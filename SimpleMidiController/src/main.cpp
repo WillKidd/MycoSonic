b61b5c63dc967316bdfd4fb84b312d02115412bf
Jan Zimmermann <Janzoo1234@gmail.com>
2023-11-20 19:20:39 +0100

    changed parameter list

--- SimpleMidiController/src/main.cpp
+++ SimpleMidiController/src/main.cpp
@@ -3,20 +3,20 @@
 // Create and bind the MIDI interface to the default hardware Serial port
 MIDI_CREATE_DEFAULT_INSTANCE();
 
-unsigned long t1 = millis();
+//unsigned long t1 = millis();
 
-void handleNoteOn(byte inChannel, byte inNumber, byte inVelocity)
+void handleNoteOn(byte inNoteNumber, byte inVelocity, byte inChannel)
 {
   Serial.print("NoteOn  ");
-  Serial.print(inNumber);
+  Serial.print(inNoteNumber);
   Serial.print("\tvelocity: ");
   Serial.println(inVelocity);
 }
 
-void handleNoteOff(byte inChannel, byte inNumber, byte inVelocity)
+void handleNoteOff(byte inNoteNumber, byte inVelocity, byte inChannel)
 {
   Serial.print("NoteOff ");
-  Serial.print(inNumber);
+  Serial.print(inNoteNumber);
   Serial.print("\tvelocity: ");
   Serial.println(inVelocity);
 }
@@ -34,17 +34,29 @@ void setup()
 }
 
 // -----------------------------------------------------------------------------
-//
+// MIDI Structor
+//[8bytes][8bytes][8bytes]
+//[Status][Data1][Data2]
+//[0-15 type; 1-16 channel;][0-127 note;][0-127 pressure;]
 // -----------------------------------------------------------------------------
 void loop()
 {
   MIDI.read();
 
-  if ((millis() - t1) > 500)
-  {
-    t1 = millis();
 
-    MIDI.sendNoteOn(27, 55, 1);
-    MIDI.sendNoteOff(27, 55, 1);
-  }
+  MIDI.sendNoteOn(20, 55, 1);
+  delay(200);
+  MIDI.sendNoteOn(40, 55, 1);
+  delay(200);
+  MIDI.sendNoteOn(80, 55, 1);
+  delay(200);
+  MIDI.sendNoteOn(20, 55, 1);
+  delay(200);
+  MIDI.sendNoteOn(40, 55, 1);
+  delay(200);
+  MIDI.sendNoteOn(80, 55, 1);
+  delay(1000);
+  MIDI.sendNoteOff(80, 55, 1);
+  MIDI.sendNoteOff(40, 55, 1);
+  MIDI.sendNoteOff(20, 55, 1);
 }
