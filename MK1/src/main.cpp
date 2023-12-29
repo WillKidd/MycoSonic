bc03d265dfb27931591fb1fed6bfa1ed13ddde04
Jan Zimmermann <Janzoo1234@gmail.com>
2023-12-29 03:46:21 +0100

    removed top number for now, as it isn't very useful the way it was used

--- MK1/src/main.cpp
+++ MK1/src/main.cpp
@@ -4,7 +4,6 @@
 #include "mozzi-audio.h"
 #include "audio-math.h"
 
-
 const int bioSensorPin = A0;
 int filterIndex = 0;
 bool useSDCardData = false;
@@ -18,8 +17,6 @@ bool isNotePlaying = true;
 int bpm = 120; // Example BPM
 int beatUnit = 4; // Assuming 4/4 time signature
 int defaultBeatUnit = 4; // Default beat unit for the time signature
-int topNumber = 4; // Top number of the time signature (e.g., 4 in 4/4 time)
-int currentBeat = 0; // Current beat within the measure
 
 // User-settable note and break durations (as note types, e.g., 4 for quarter note)
 int noteDurationType = 4; // Quarter note
@@ -39,7 +36,8 @@ void setup() {
 
   // Calculate durations based on user-set values
   noteDuration = noteDurationToTime(noteDurationType, bpm, beatUnit, defaultBeatUnit);
-  breakDuration = noteDurationToTime(breakDurationType, bpm, beatUnit, defaultBeatUnit);
+  breakDuration = 0.0;
+  //breakDuration = noteDurationToTime(breakDurationType, bpm, beatUnit, defaultBeatUnit);
 
   lastChangeTime = millis();
 }
@@ -53,34 +51,17 @@ void updateControl(){
   unsigned long currentTime = millis();
   switch (currentState) {
     case PLAYING:
-      if (millis() - lastChangeTime >= noteDuration) {
-        currentBeat++;  // Increment the current beat count
-
-        // Check if the end of the measure is reached
-        if (currentBeat >= topNumber) {
-          currentBeat = 0;  // Reset beat count at the start of a new measure
-          // Optionally, implement actions that should occur at the start of a new measure
-        }
-
-        // Check if breaks are implemented and have duration
-        if (breakDuration > 0) {
-          setSineWaveFrequency(0);  // Silence the output for a break
-          currentState = BREAK;     // Change state to BREAK
-        } else {
-          // Start the next note if no break duration
-          bioValue = analogRead(bioSensorPin);
-          dataValue = applyFilter(bioValue, filterIndex);
-          float freq = harmonicMapping(dataValue, 0, 1023, 220);
-          setSineWaveFrequency(freq);  // Set frequency for the next note
-          lastChangeTime = millis();   // Update the time of the last change
-        }
+      if (currentTime - lastChangeTime >= noteDuration) {
+        setSineWaveFrequency(0); // Silence the output for a break
+        currentState = BREAK;
+        lastChangeTime = currentTime;
       }
       break;
     case BREAK:
       if (currentTime - lastChangeTime >= breakDuration) {
         bioValue = analogRead(bioSensorPin);
         dataValue = applyFilter(bioValue, filterIndex);
-        float freq = harmonicMapping(dataValue, 0, 1023, 220);
+        float freq = harmonicMapping(dataValue, 0, 1023, 110);
         setSineWaveFrequency(freq);
         currentState = PLAYING;
         lastChangeTime = currentTime;
@@ -92,6 +73,4 @@ void updateControl(){
 int updateAudio(){
   return updateSineWave();
 }
-
-// Add the bpmToMilliseconds and noteDurationToTime functions here
 
