d4de49da3c11c00c28a33b6959bccb43db748c54
Jan Zimmermann <Janzoo1234@gmail.com>
2024-01-10 20:42:27 +0100

    fitted to changes

--- MK1/src/main.cpp
+++ MK1/src/main.cpp
@@ -33,38 +33,102 @@ State currentState = PLAYING;
 BaseWaveform* currentWaveform = nullptr;
 int currentFrequency = 220;
 
-const int OK_BUTTON_PIN = 2;
-const int EDIT_BUTTON_PIN = 3;
-const int BACK_BUTTON_PIN = 4;
-const int TOGGLE_BUTTON_PIN = 5;
-const int ROTARY_PIN_A = 6;
-const int ROTARY_PIN_B = 7;
+const int OK_BUTTON_PIN = 5;
+const int EDIT_BUTTON_PIN = 4;
+const int BACK_BUTTON_PIN = 2;
+const int TOGGLE_BUTTON_PIN = 6;
+const int ROTARY_PIN_A = 7;
+const int ROTARY_PIN_B = 8;
 
 InputHandler inputHandler(OK_BUTTON_PIN, EDIT_BUTTON_PIN, BACK_BUTTON_PIN, TOGGLE_BUTTON_PIN, ROTARY_PIN_A, ROTARY_PIN_B);
 MenuItem rootItem("Main Menu", BASE_ITEM);
-MenuHandler menuHandler(&rootItem); // Assuming this is already created
-LCDHandler lcdHandler(0x27, 16, 2); // Assuming this is already created
+MenuHandler menuHandler(&rootItem);
 
+
+void updateDisplay(){
+    menuHandler.displayCurrentItem();
+}
 
 void setup() {
   Serial.begin(9600);
   initSDCard();
   startMozzi(64);
-  // Calculate durations based on user-set values
+
   noteDuration = noteDurationToTime(noteDurationType, bpm, beatUnit, defaultBeatUnit);
-  //breakDuration = 0.0;
   breakDuration = noteDurationToTime(breakDurationType, bpm, beatUnit, defaultBeatUnit);
 
   currentWaveform = new SawWave(currentFrequency);
   lastChangeTime = millis();
+  LCDHandler lcdHandler(0x27, 16, 2);
+  lcdHandler.init();
+  menuHandler.setLCDHandler(&lcdHandler);
+  updateDisplay();
 }
 
 void loop(){
   audioHook();
 }
 
+
 void updateControl(){
   unsigned long currentTime = millis();
+  
+  inputHandler.update();
+  if (inputHandler.isOkPressed()) {
+    Serial.println("OK");
+    if (menuHandler.isEditMode()) {
+        menuHandler.saveChanges(); // Save changes if in edit mode
+        menuHandler.exitEditMode(); // Exit edit mode
+    }
+  }
+
+  else if (inputHandler.isEditPressed()) {
+      Serial.println("EDIT");
+
+    if (menuHandler.isEditMode()) {
+        menuHandler.exitEditMode(); // Exit edit mode
+    } else {
+        menuHandler.enterEditMode(); // Enter edit mode
+    }
+  }
+
+  else if (inputHandler.isBackPressed()) {
+      Serial.println("BACK");
+    if (menuHandler.isEditMode()) {
+        menuHandler.discardChanges(); // Discard changes if in edit mode
+        menuHandler.exitEditMode(); // Exit edit mode
+    } else {
+        menuHandler.back(); // Navigate back in the menu
+    }
+  }
+  else if (inputHandler.isToggleButtonPressed()) {
+    Serial.println("TOGGLE");
+    menuHandler.toggleCurrentItem(); // Toggle the current item
+  }
+/*   else if (inputHandler.isRotaryIncremented()) {
+      Serial.println("UP");
+    if (menuHandler.isEditMode()) {
+        menuHandler.updateParameterValue(0.1); // Increase editable parameter
+    } else {
+        menuHandler.nextItem(); // Navigate to the next item
+    }
+  } */
+
+/*     // Handle Rotary Encoder Decrements
+  else if (inputHandler.isRotaryDecremented()) {
+    Serial.println("DOWN");
+    if (menuHandler.isEditMode()) {
+        menuHandler.updateParameterValue(-0.1); // Decrease editable parameter
+    } else {
+        menuHandler.previousItem(); // Navigate to the previous item
+    }
+
+  }*/
+
+/*   if (changed){
+    updateDisplay();
+  }  */
+  
   switch (currentState) {
     case PLAYING:
       if (currentTime - lastChangeTime >= noteDuration) {
@@ -78,65 +142,15 @@ void updateControl(){
         bioValue = analogRead(bioSensorPin);
         dataValue = applyFilter(bioValue, filterIndex);
         float freq = harmonicMapping(dataValue, 0, 1023, 110);
-        currentWaveform->setFrequency(freq);
+        //currentWaveform->setFrequency(freq);
+        currentWaveform->setFrequency(110); //for testing not as annoying
         currentState = PLAYING;
         lastChangeTime = currentTime;
       }
       break;
-  }
-
-  inputHandler.update();
-  if (inputHandler.isOkPressed()) {
-    if (menuHandler.isEditMode()) {
-        menuHandler.saveChanges(); // Save changes if in edit mode
-        menuHandler.exitEditMode(); // Exit edit mode
-    }
-  }
-
-  // Handle Edit Button Press - Toggle Edit Mode
-  if (inputHandler.isEditPressed()) {
-      if (menuHandler.isEditMode()) {
-          menuHandler.exitEditMode(); // Exit edit mode
-      } else {
-          menuHandler.enterEditMode(); // Enter edit mode
-      }
-  }
-
-  // Handle Back Button Press - Discard Changes or Navigate Back
-  if (inputHandler.isBackPressed()) {
-      if (menuHandler.isEditMode()) {
-          menuHandler.discardChanges(); // Discard changes if in edit mode
-          menuHandler.exitEditMode(); // Exit edit mode
-      } else {
-          menuHandler.back(); // Navigate back in the menu
-      }
-  }
-
-  // Handle Toggle Button Press - Toggle Current Item
-  if (inputHandler.isToggleButtonPressed()) {
-      menuHandler.toggleCurrentItem(); // Toggle the current item
-  }
-
-  // Handle Rotary Encoder Increments
-  if (inputHandler.isRotaryIncremented()) {
-      if (menuHandler.isEditMode()) {
-          menuHandler.updateParameterValue(0.1); // Increase editable parameter
-      } else {
-          menuHandler.nextItem(); // Navigate to the next item
-      }
-  }
-
-  // Handle Rotary Encoder Decrements
-  if (inputHandler.isRotaryDecremented()) {
-      if (menuHandler.isEditMode()) {
-          menuHandler.updateParameterValue(-0.1); // Decrease editable parameter
-      } else {
-          menuHandler.previousItem(); // Navigate to the previous item
-      }
   }
 }
 
 int updateAudio(){
   return currentWaveform->update();
 }
-
